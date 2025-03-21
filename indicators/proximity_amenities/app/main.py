0a1cef537b9d2e1f799d58490c1e6c7d77d711a2
Zetaso <zetasodev@gmail.com>
2025-03-21 12:21:59 -0300

    amenity proximity indicator (variable)

---
+++ indicators/proximity_amenities/app/main.py
@@ -0,0 +1,8 @@
+from indicators.proximity_bus_stops.app.indicator_base import Indicator
+
+def main():
+    indicator = Indicator()
+    indicator.execute()
+
+if __name__ == '__main__':
+    main()
